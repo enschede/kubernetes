d7d57a21bb2df96944ec1544dd8d1e0f9b1ef956
Marc Enschede <marc@marcenschede.nl>
2020-08-30 21:19:50 +0200

    EKS

--- module1/src/eks/eks-helm.sh
+++ module1/src/eks/eks-helm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 kubectl create serviceaccount tiller \
-    --namespace kube-system
+  --namespace kube-system
 kubectl create clusterrolebinding tiller-admin-binding \
-    --clusterrole=cluster-admin \
-    --serviceaccount=kube-system:tiller
+  --clusterrole=cluster-admin \
+  --serviceaccount=kube-system:tiller
 helm init --service-account=tiller
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
 helm repo update
