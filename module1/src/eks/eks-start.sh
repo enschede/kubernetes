43317a75842235fe28da7999396c2fad0ccbc003
Marc Enschede <marc@marcenschede.nl>
2020-09-27 22:59:19 +0200

    eksctl uitgebreid

--- module1/src/eks/eks-start.sh
+++ module1/src/eks/eks-start.sh
@@ -6,30 +6,59 @@ kind: ClusterConfig
 metadata:
   name: enschede
   region: us-west-2
+  version: "1.17"
+#vpc:
+#  autoAllocateIPV6: true     # Doet niks
 nodeGroups:
+# managedNodeGroups:
   - name: ng-1
     instanceType: t3.medium
     desiredCapacity: 2
     minSize: 1
     maxSize: 4
+    ssh:
+      allow: true
     iam:
       # polices added to worker node role
       withAddonPolicies:
-        # ecr access
-        imageBuilder: true
+        imageBuilder: true      # ecr access
         autoScaler: true
-        # allows read/write to zones in Route53
-        externalDNS: true
+        externalDNS: true       # allows read/write to zones in Route53
         certManager: true
         appMesh: true
         appMeshPreview: true
         ebs: true
         fsx: true
         efs: true
-        # required for ALB-ingress
-        albIngress: true
+        albIngress: true        # required for ALB-ingress
         xRay: true
         cloudWatch: true
+#nodeGroups:
+#  - name: ng-2
+#    instanceType: t3.medium
+#    desiredCapacity: 2
+#    minSize: 1
+#    maxSize: 4
+#    ssh:
+#      allow: true
+#    iam:
+#      # polices added to worker node role
+#      withAddonPolicies:
+#        # ecr access
+#        imageBuilder: true
+#        autoScaler: true
+#        # allows read/write to zones in Route53
+#        externalDNS: true
+#        certManager: true
+#        appMesh: true
+#        appMeshPreview: true
+#        ebs: true
+#        fsx: true
+#        efs: true
+#        # required for ALB-ingress
+#        albIngress: true
+#        xRay: true
+#        cloudWatch: true
 cloudWatch:
   clusterLogging:
     enableTypes: ["*"]
@@ -37,7 +66,7 @@ EOF
 
 #eksctl utils update-cluster-logging --region=us-west-2 --cluster=enschede
 
-./logging.sh
+./eks-logging.sh
 ./eks-helm.sh
 ./eks-dashboard.sh
-./eks-ingress.sh
+#./eks-ingress-controller.sh
