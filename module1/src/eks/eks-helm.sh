37a917a521944fc516bce936b366e54829b8ec4b
Marc Enschede <marc@marcenschede.nl>
2020-11-17 11:21:33 +0100

    EKS Classic LB vervangen door ALB

--- module1/src/eks/eks-helm.sh
+++ module1/src/eks/eks-helm.sh
@@ -6,5 +6,5 @@ kubectl create clusterrolebinding tiller-admin-binding \
   --clusterrole=cluster-admin \
   --serviceaccount=kube-system:tiller
 helm init --service-account=tiller
-helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+
 helm repo update
--- module1/src/eks/eks-ingress-controller.sh
+++ module1/src/eks/eks-ingress-controller.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
 CLUSTER_NAME=enschede
+AWS_ACCOUNT_ID=
+AWS_REGION=us-west-2
 
-helm install incubator/aws-alb-ingress-controller \
+# https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
+helm repo add eks https://aws.github.io/eks-charts
+kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
+
+eksctl utils associate-iam-oidc-provider \
+  --region $AWS_REGION \
+  --cluster $CLUSTER_NAME \
+  --approve
+curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
+aws iam create-policy \
+  --policy-name AWSLoadBalancerControllerIAMPolicy \
+  --policy-document file://iam-policy.json
+eksctl create iamserviceaccount \
+  --cluster=$CLUSTER_NAME \
+  --namespace=kube-system \
+  --name=aws-load-balancer-controller \
+  --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy \
+  --approve
+
+#helm install eks/aws-load-balancer-controller
+helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller \
+  --namespace kube-system \
   --set clusterName=$CLUSTER_NAME \
-  --set autoDiscoverAwsRegion=true \
-  --set autoDiscoverAwsVpcID=true
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=aws-load-balancer-controller
